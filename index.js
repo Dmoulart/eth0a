682b63f5b27330bdb3cb19ae7948791ce397d07b
Dorian <dorian@resalocal.com>
2022-07-18 14:27:57 +0200

    Add socket

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Setup socket.Io
+const { Server } = require("socket.io");
+
+app.use(express.static("public"));
+
+// Give access socket io code client side
+app.get("/socket.io/socket.io.js", (req, res) => {
+  res.sendFile(__dirname + "/node_modules/socket.io/client-dist/socket.io.js");
+});
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
