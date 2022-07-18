5801786fef20ef0b6e2369e57ccf07152d627993
Dorian <dorian@resalocal.com>
2022-07-18 14:32:05 +0200

    Setup basic chat

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("chat:message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat:message", msg);
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
